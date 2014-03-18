6d496f247dbfe3f4f2dffd52d2e7e4d9085172c2
Wilfredo Mejia <itha92@gmail.com>
2014-03-17 18:00:14 -0700

    initial commit 1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Neoterminal2::Application.config.secret_key_base = 'e06a100b520c74ac565ea8f9cf1cb45e7709335bdd042cfb5e1a923cfd5eb9b58a3663775d3921766c40c7ae6b49ea85f1c6c29ff7405a86e554b9ba0ffd794e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Neoterminal2::Application.config.session_store :cookie_store, key: '_Neoterminal2_session'
