0c94750b0435a10f621e6e8fc492caa26b935d9c
ithamar <itha92@gmail.com>
2014-03-25 20:30:35 -0600

    AntedDeQueReyLoArruine

--- app/models/autobus.rb
+++ app/models/autobus.rb
@@ -2,4 +2,5 @@ class Autobus < ActiveRecord::Base
 	has_many :asiento
 	has_one :empleado
 	has_many :mantenimiento
+	has_many :itinerario
 end
--- app/models/itinerario.rb
+++ app/models/itinerario.rb
@@ -1,4 +1,5 @@
 class Itinerario < ActiveRecord::Base
   belongs_to :terminal
   has_many :boleto
+  belongs_to :autobus
 end
